4351192193b319556dc5f57d2b25c4bd766ea4b0
Jepe <unityjepe@gmail.com>
2018-11-06 13:56:30 +0100

    .

--- Assets/_GameAssets/Scripts/MarcianoScript.cs
+++ Assets/_GameAssets/Scripts/MarcianoScript.cs
@@ -5,88 +5,55 @@ using UnityEngine.UI;
 
 public class MarcianoScript : MonoBehaviour {
     [SerializeField] LayerMask floorLayer;
-    bool haciaDerecha = true;
     [SerializeField] Transform posPies;
     [SerializeField] Text txtPuntuacion;
-    [SerializeField] Text txtPuntuacionVida;
     [SerializeField] float speed = 5;
     [SerializeField] float jumpForce = 1;
     int vidasMaximas = 3;
     [SerializeField] int vidas;
-    [SerializeField] int vidaMax=100;
     [SerializeField] int puntos = 0;
     [SerializeField] float radioOverlap = 0.1f;
-    [SerializeField] Animator playerAnimator;
+    Animator playerAnimator;
     Rigidbody2D rb2D;
     bool saltando = false;
-    
+
     private void Start() {
         rb2D = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
         txtPuntuacion.text = "Score:" + puntos.ToString();
-        txtPuntuacionVida.text = "Vida:" + vidaMax.ToString();
-
     }
-
-    private bool EstaEnElSuelo() {
-        bool enSuelo = false;
-        Collider2D col = Physics2D.OverlapCircle(posPies.position, radioOverlap, floorLayer);
-        if (col != null) {
-            enSuelo = true;
-        }
-        return enSuelo;
-    }
-
 
     private void Update() {
         if (Input.GetKey(KeyCode.Space)) {
             saltando = true;
         }
     }
-    void FixedUpdate () {
-        
+
+    void FixedUpdate() {
         float xPos = Input.GetAxis("Horizontal");
-        float yPos = Input.GetAxis("Vertical");
         float ySpeedActual = rb2D.velocity.y;
 
         if (Mathf.Abs(xPos) > 0.01f) {
-            playerAnimator.SetBool("Andar", true);
-           
-           
-        }
-        
-        
-        else {
-            playerAnimator.SetBool("Andar", false);
-            
-           
-
+            playerAnimator.SetBool("andando", true);
+        } else {
+            playerAnimator.SetBool("andando", false);
         }
 
-
-       // if (Mathf.Abs(xPos) > 0.01f ){
-            print("POR AQUI");
-            if(saltando) {
-                saltando = false;
-                if (EstaEnElSuelo()) {
-                    rb2D.velocity = new Vector2(xPos * speed, jumpForce);
-                } else {
-                    rb2D.velocity = new Vector2(xPos * speed, ySpeedActual);
-                }
-            } else if (Mathf.Abs(xPos) > 0.01f) {
+        if (saltando) {
+            saltando = false;
+            if (EstaEnElSuelo()) {
+                rb2D.velocity = new Vector2(xPos * speed, jumpForce);
+            } else {
                 rb2D.velocity = new Vector2(xPos * speed, ySpeedActual);
             }
-       // }
-        
+        } else if (Mathf.Abs(xPos) > 0.01f) {
+            rb2D.velocity = new Vector2(xPos * speed, ySpeedActual);
+        }
     }
-  
 
     public void IncrementarPuntuacion(int puntosAIncrementar) {
         puntos = puntos + puntosAIncrementar;
         txtPuntuacion.text = "Score:" + puntos.ToString();
-    }
-    public void QuitarVida(int VidaAQuitar) {
-        vidaMax = vidaMax - VidaAQuitar;
-        txtPuntuacionVida.text = "Vida:" + vidaMax.ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -94,13 +61,29 @@ public class MarcianoScript : MonoBehaviour {
             IncrementarPuntuacion(1);
             Destroy(collision.gameObject);
         }
-        if (collision.gameObject.CompareTag("Pinchos")) {
-            QuitarVida(5);
-
-            
-        }
     }
-    
-        
-    
+
+    private bool EstaEnElSuelo() {
+        bool enSuelo = false;
+        Collider2D colider = Physics2D.OverlapCircle(posPies.position, radioOverlap, floorLayer);
+        if (colider != null) {
+            enSuelo = true;
+        }
+        return enSuelo;
+    }
+
+    /*
+    //Version basada en TAG y utilizando OverlapCircleAll
+    private bool EstaEnElSuelo() {
+    bool enSuelo = false;
+    Collider2D[] cols = Physics2D.OverlapCircleAll(posPies.position, radioOverlap);
+    for (int i = 0; i < cols.Length; i++) {
+    if (cols[i].gameObject.tag == "Suelo") {
+    enSuelo = true;
+    break;
+    }
+    }
+    return enSuelo;
+    }
+    */
 }
---
+++ Assets/_GameAssets/Scripts/MoscaScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoscaScript : MonoBehaviour {
+    public Transform limiteDerecho;
+    public Transform limiteIzquierdo;
+    bool haciaDerecha = false;
+    private void Start() {
+        transform.position = limiteDerecho.position;
+    }
+    void Update () {
+        if (haciaDerecha == true) {
+            transform.Translate(Vector2.right * Time.deltaTime);
+            if (transform.position.x > limiteDerecho.position.x) {
+                haciaDerecha = false;
+                CambiarOrientacion();
+            }
+        } else {
+            transform.Translate(Vector2.left* Time.deltaTime);
+            if (transform.position.x < limiteIzquierdo.position.x) {
+                haciaDerecha = true;
+                CambiarOrientacion();
+            }
+        }
+	}
+    void CambiarOrientacion () {
+        if (haciaDerecha) {
+            transform.localScale = new Vector2(-1, 1);
+        } else {
+            transform.localScale = new Vector2(1, 1);
+        }
+        
+    }
+}
